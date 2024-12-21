3387e53c028f560e844bb5bd4a129fb9b89346a6
bense <bense@gmail.com>
2024-12-21 21:09:48 +0800

    security_bars

--- thsdata/ths.py
+++ thsdata/ths.py
@@ -1,7 +1,9 @@
 from .quote_lib import QuoteLib
 from .util import *
 from .model import Reply
+from .constants import *
 import re
+import datetime
 
 ZipVersion = "2"
 
@@ -44,5 +46,46 @@ class ThsQuote:
 
         if fields:
             reply.data = [entry for entry in reply.data if all(field in entry for field in fields)]
+
+        return reply
+
+    def security_bars(self, code: str, start: int, end: int, fuquan: str, period: int):
+
+        valid_fuquan = {Fuquanqian, Fuquanhou, FuquanNo}
+        valid_periods = {Kline1m, Kline5m, Kline15m, Kline30m, Kline60m, Kline120m, KlineDay, KlineWeek, KlineMoon,
+                         KlineQuarterly, KlineYear}
+
+        if fuquan not in valid_fuquan:
+            raise ValueError("Invalid fuquan.")
+
+        if period not in valid_periods:
+            raise ValueError("Invalid period.")
+
+        mPeriod = {Kline1m, Kline5m, Kline15m, Kline30m, Kline60m, Kline120m}
+
+        if len(code) != 10:
+            raise ValueError("Code must be 10 characters long and start with 'USHA' or 'USZA'.")
+
+        instance = rand_instance(8)
+        zipVersion = ZipVersion
+        data_type = "1,7,8,9,11,13,19"
+        market = code[:4]
+        short_code = code[4:]
+        req = f"id=210&instance={instance}&zipversion={zipVersion}&code={short_code}&market={market}&start={start}&end={end}&fuquan={fuquan}&datatype={data_type}&period={period}"
+        response = self.quotelib.query_data(req)
+        if response == "":
+            raise ValueError("No history data found.")
+
+        reply = Reply(response)
+        reply.convert_data()
+
+        if period in mPeriod:
+            for entry in reply.data:
+                if "time" in entry:  # 检查是否存在 "time" 键
+                    entry["time"] = ths_int2time(entry["time"])
+        else:
+            for entry in reply.data:
+                if "time" in entry:  # 检查是否存在 "time" 键
+                    entry["time"] = datetime.datetime.strptime(str(entry["time"]), "%Y%m%d")
 
         return reply
